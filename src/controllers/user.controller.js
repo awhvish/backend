9a3b310f2edc025cb9a9c85d93d1437cfe2ec3e3
root <max@kali>
2024-06-22 17:12:24 +0530

    controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,21 +16,25 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
-  console.log(fullName, email, username, password);
 
   if (fullName === "" || email === "" || username === "" || password === "") {
     throw new ApiError(400, "All fields are required");
   } //check validation for emails as well later
 
   //check if user already exists
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
@@ -43,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Failed to upload avatar");
   }
 
-  const user = User.create({
+  const user =  await User.create({
     fullName,
     email,
     username: username.toLowerCase(),
@@ -52,7 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage?.url || "",
   }); //create user object - create entry in db
 
-  const createdUser = await User.find(user._id).select("-password -refreshToken");
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
   if(!createdUser) {  
     throw new ApiError(500, "Failed to create user");
   }
